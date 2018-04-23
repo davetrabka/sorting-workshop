e2bb7a5f3009eb5934931b6f9ece63762ef1730b
Dave Trabka <dtrabka@gmail.com>
2018-04-23 15:07:44 -0400

    added merge sort files

--- bubblesort.js
+++ bubblesort.js
@@ -1,14 +1,13 @@
 let swapCount;
 
 function bubbleSort(arr) {
-
     if (swapCount === 0) {
         return arr;
     } else {
         swapCount = 0;
         for (let i = 0; i < arr.length - 1; i++) {
             if (arr[i + 1] < arr[i]) {
-                let temp = arr[i]
+                let temp = arr[i];
                 arr[i] = arr[i + 1];
                 arr[i + 1] = temp;
                 swapCount++;
